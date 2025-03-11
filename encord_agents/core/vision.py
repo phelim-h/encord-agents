c70373b8c6cbed64be2ea7335438701bb5b3571a
Frederik Hvilshøj <93145535+frederik-encord@users.noreply.github.com>
2025-03-11 14:41:36 +0100

    fix: base64 bgr not rgb when using cv2 (#111)
    * fix: cv2 base64 encoding expects bgr frames but we pass rgb frames around
    
    Co-authored-by: Jim-Encord <156204297+Jim-Encord@users.noreply.github.com>

--- encord_agents/core/vision.py
+++ encord_agents/core/vision.py
@@ -111,5 +111,16 @@ def crop_to_object(image: NDArray[np.uint8], coordinates: CroppableCoordinates) 
 
 
 def b64_encode_image(img: NDArray[np.uint8], format: Base64Formats = ".jpg") -> str:
-    _, encoded_image = cv2.imencode(format, img)
+    """
+    Encode an image to a base64 string.
+
+    Args:
+        img: The image to encode. Expects [RGB] channels
+        format: The format of the image.
+
+    Returns:
+        The base64 encoded image.
+    """
+    coerced_color_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    _, encoded_image = cv2.imencode(format, coerced_color_img)
     return base64.b64encode(encoded_image).decode("utf-8")  # type: ignore
